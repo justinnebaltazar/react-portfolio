86dad995fdb928b912e6467c1c10592b470690f7
Justinne Baltazar <jfb3@sfu.ca>
2024-12-29 13:13:40 -0800

    Add responsiveness for navbar and create navigation button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,23 +5,32 @@ import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils"
  
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className={styles.navbar}>
             <a className={styles.title} href="/">Portfolio</a>
             <div className={styles.menu}>
-                <img className={styles.menuBtn} src="./assets/nav/menuIcon.png" alt="menu-button"></img>
-                <ul className={styles.menuItems}>
+                <img className={styles.menuBtn} src={
+                    menuOpen ? "./assets/nav/closeIcon.png" : "./assets/nav/menuIcon.png"} 
+                    alt="menu-button"
+                    onClick={() => {
+                        setMenuOpen(!menuOpen);
+                    }}>
+                    </img>
+                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick={() => setMenuOpen(false)}>
                     <li>
-                        <a href="/">About</a>
+                        <a href="#about">About</a>
                     </li>
                     <li> 
-                        <a href="/">Projects</a>
+                        <a href="#projects">Projects</a>
                     </li>
                     <li>
-                        <a href="/">Experience</a>
+                        <a href="#experience">Experience</a>
                     </li>
                     <li>
-                        <a href="/">Contact</a>
+                        <a href="#about">Contact</a>
                     </li>
                 </ul>
             </div>

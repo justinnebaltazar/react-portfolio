d2174cb29564a59c4746aef7a4a1ef7d7651e6a6
Justinne Baltazar <jfb3@sfu.ca>
2024-12-29 15:16:53 -0800

    Create Experience component with boilerplate and add to App

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Experience.module.css";
+
+export const Experience = () => {
+    return (
+        <section>
+            
+        </section>
+    )
+};

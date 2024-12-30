12c6814379217f96381335455a5bcad5fce75da6
Justinne Baltazar <jfb3@sfu.ca>
2024-12-29 16:05:41 -0800

    Update Experience component with styling and update url function

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "./Experience.module.css";
+import skills from "../../data/skills.json";
+import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
     return (
-        <section>
-            
+        <section className={styles.container} id="experience">
+            <h2 className={styles.title}>Skills</h2>
+            <div className={styles.content}>
+                <div className={styles.skills}>{
+                    skills.map((skill, id) => {
+                        return <div className={styles.skill} key={id}>
+                            <div className={styles.skillImageContainer}><img src={getImageUrl(skill.imageSrc)} alt={skill.title}></img></div>
+                            <p>{skill.title}</p>
+                        </div>
+
+                    })
+                }</div>
+            </div>
         </section>
     )
 };

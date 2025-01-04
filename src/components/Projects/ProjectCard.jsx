17f196451927d39b91d4bc5c7aa7059bddeeafde
Justinne Baltazar <jfb3@sfu.ca>
2025-01-03 23:43:04 -0800

    Update content, sizing, and format of project cards

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -2,23 +2,28 @@ import React from "react";
 import { getImageUrl } from "../../utils";
 import styles from "./ProjectCard.module.css";
 
-export const ProjectCard = ({ project : {title, imageSrc, description, skills, demo, source}}) => {
+export const ProjectCard = ({ project : {title, projectType, imageSrc, description, skills, demo, source}}) => {
     return (
         <div className={styles.container}>
-            <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image}/>
-            <h3 className={styles.title}>{title}</h3>
-            <p className={styles.description}>{description}</p>
-            <ul className={styles.skills}>
-                {skills.map((skill, id) => {
-                    return (
-                        <li className={styles.skill} key={id}>{skill}</li>
-                    )})
-                }
-            </ul>
-            <div className={styles.links}>
-                <a className={styles.link} href={demo}>Demo</a>
-                <a className={styles.link} href={source}>Source</a>
+            <div className={styles.content}>
+                <h3 className={styles.title}>{title}</h3>
+                <h4 className={styles.projectType}>{projectType}</h4>
+                <p className={styles.description}>{description}</p>
+                <ul className={styles.skills}>
+                    {skills.map((skill, id) => {
+                        return (
+                            <li className={styles.skill} key={id}>{skill}</li>
+                        )})
+                    }   
+                </ul>
+
+                <div className={styles.links}>
+                        <a className={styles.link} href={demo}>Demo</a>
+                        <a className={styles.link} href={source}>Source</a>
+                </div>
             </div>
+            <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image}/>
+
         </div>
     )
 }

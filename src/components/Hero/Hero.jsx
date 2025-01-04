e10acc284ca6f31570c494e8d48428d42d5b791e
Justinne Baltazar <jfb3@sfu.ca>
2025-01-03 17:47:08 -0800

    Update hero styling and added links

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
     return (
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>
-                    Hi, I am Justinne
+                    Justinne Baltazar
                 </h1>
-                <p className={styles.description}>I'm a full-time student at Simon Fraser University, studying Computing Science. Reach out if you'd like to learn more.</p>
-                <a className={styles.contactBtn} href="jfb3@sfu.ca">Contact Me</a>
+                <p className={styles.description}>An aspiring software developer with an interest in game development and computer systems</p>
+                <ul className={styles.links}>
+                    <li><a href="https://github.com/justinnebaltazar"><img src={getImageUrl("contact/githubIcon.png")} alt="github-icon" /></a></li>
+                    <li><a href="https://www.linkedin.com/in/justinnebaltazar/"><img src={getImageUrl("contact/linkedinIcon.png")} alt="github-icon" /></a></li>
+                    <li><a href="mailto:jfb3@sfu.ca"><img src={getImageUrl("contact/emailIcon.png")}></img></a></li>
+                </ul>
             </div>
-            <img className={styles.heroImg} src="./assets/hero/heroImage.png" alt="hero-image-of-me"></img>
             <div className={styles.topBlur}></div>
             <div className={styles.bottomBlur}></div>
         </section>

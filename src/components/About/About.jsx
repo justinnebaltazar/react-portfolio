7c92e0c4ccc2c32cda9b381aeaf610aef35d3385
Justinne Baltazar <jfb3@sfu.ca>
2025-01-11 23:09:00 -0800

    Update About component styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -20,14 +20,16 @@ export const About = () => {
                     </div>
                 </div>
 
-                <p>Hi, my name is Justinne! I'm a Computing Science student at Simon Fraser University.<br/></p>
+                <p className={styles.intro}>Hi, my name is Justinne! I'm a Computing Science student at Simon Fraser University.<br/></p>
                 
                 <p> <br/> Currently, I work part-time as a Tutor and volunteer as a Coding Instructor. As a tutor, I teach students from Grades 2 to 12 in subject areas such as, Math, English, and Chemistry. 
                 As a Coding Instructor, I teach programming and computer science topics to neurodivergent children. My experience as a tutor and coding instructor has allowed me 
                 to master rudimentary mathematical and programming concepts. It also developed my communication and problem-solving abilities.
                 </p>
                 
-                <p>Outside of school and work, I...</p>
+                <p> <br/> Outside of school and work, I enjoy thrifting, visiting record stores, reading, going to the gym, hiking, journaling, photography,
+                    and learning new languages such as German and French.
+                </p>
             </div>
         </section>
     )

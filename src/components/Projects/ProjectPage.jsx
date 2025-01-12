f4c9e9515d4941c3ad84806c9dd90b5b3139fb8f
Justinne Baltazar <jfb3@sfu.ca>
2025-01-11 23:24:21 -0800

    Create ProjectPage component

---
+++ src/components/Projects/ProjectPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+
+export const ProjectPage = ({ project : {title, projectType, imageSrc, description, skills, demo, source}}) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <h3 className={styles.title}>{title}</h3>
+                <h4 className={styles.projectType}>{projectType}</h4>
+                <p className={styles.description}>{description}</p>
+                <ul className={styles.skills}>
+                    {skills.map((skill, id) => {
+                        return (
+                            <li className={styles.skill} key={id}>{skill}</li>
+                        )})
+                    }   
+                </ul>
+
+                <div className={styles.links}>
+                        <a className={styles.link} href={demo}>Demo</a>
+                        <a className={styles.link} href={source}>Source</a>
+                </div>
+            </div>
+        </div>
+    )    
+}

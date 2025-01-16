664d3818a365c9ec28bfc0cea52badf231d5c4f6
Justinne Baltazar <jfb3@sfu.ca>
2025-01-15 23:47:55 -0800

    Create component to test project routes

---
+++ src/components/Projects/Undercooked.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Undercooked = () => {
+  return (
+    <div>
+      <h1>Welcome to the Undercooked Page</h1>
+      <p>This is a project about an amazing cooking game!</p>
+    </div>
+  );
+};

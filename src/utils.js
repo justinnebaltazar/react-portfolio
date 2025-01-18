b3ebaee77f6e566b695918d44bd78cf7172b853c
Justinne Baltazar <jfb3@sfu.ca>
2025-01-18 12:17:45 -0800

    Add function for extracting project info

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,26 @@
+import projectData from "./data/projects.json";
+
 export const getImageUrl = (path) => {
     return `/assets/${path}`;
+};
+
+export const getProjectInfo = (projectTitle) => {
+    let currentProject;
+    for (let project in projectData) {
+        if (project.title === projectTitle ) {
+            // return the project and its values
+            currentProject = {
+                title: project.title, 
+                projectType: project.projectType, 
+                imageSrc: project.imageSrc, 
+                description: project.description, 
+                skills: project.skills, 
+                demo: project.demo, 
+                source: project.source, 
+                componentTitle: project.componentTitle
+            }
+        }
+    }
+
+    return currentProject;
 };
